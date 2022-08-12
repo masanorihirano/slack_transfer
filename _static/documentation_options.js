dae20ccd61646ddfeb8a79c39539851dcacb165a
masanorihirano <masanorihirano@users.noreply.github.com>
2022-08-12 11:49:00 +0000

    deploy: debb83ac5ccc0bbb45251a546ef2da08ab2bb024

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.6a7',
+    VERSION: '0.1.6a8',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
